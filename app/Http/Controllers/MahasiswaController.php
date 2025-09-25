7fddf4d708aec888037121c5a9c71f595cfe1f8d
nasywa <widia24ti@mahasiswa.pcr.ac.id>
2025-09-25 08:52:24 +0700

    ini akun nasywa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -11,7 +11,7 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        return "Menampilkan data matakuliah";
+        return "Menampilkan data haha matakuliah";
     }
 
     /**
