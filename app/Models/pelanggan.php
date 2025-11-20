98a4836a20bfd5b1969a9281a2184a223ef5a6fb
nasywa <widia24ti@mahasiswa.pcr.ac.id>
2025-11-20 08:01:50 +0700

    coba"

--- app/Models/pelanggan.php
+++ app/Models/pelanggan.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class pelanggan extends Model
+class Pelanggan extends Model
 {
     protected $table = 'pelanggan';
     protected $primaryKey = 'pelanggan_id';
@@ -13,7 +13,7 @@ class pelanggan extends Model
         'last_name',
         'birthday',
         'gender',
-        'email',
+        'emails',
         'phone',
     ];
 }
