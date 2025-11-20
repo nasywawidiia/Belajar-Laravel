41b38daf9363e73a06a4c0c6d4ecb25f07084a75
nasywawidiia <widia24ti@mahasiswa.pcr.ac.id>
2025-11-20 07:51:07 +0700

    ubah error pelanggan

--- app/Models/pelanggan.php
+++ app/Models/pelanggan.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class pelanggan extends Model
+class Pelanggan extends Model
 {
     protected $table = 'pelanggan';
     protected $primaryKey = 'pelanggan_id';
